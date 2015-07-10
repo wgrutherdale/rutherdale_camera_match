97af7d8407361caa7876c65eb6827941be71139e
William Gordon Rutherdale <will.rutherdale@utoronto.ca>
2015-07-10 19:08:57 -0400

    Added comments to all methods. Added command-line parameters and help. Output results to json. Eliminated accessory matches and old heuristics.

--- match.pl
+++ match.pl
@@ -1,31 +1,60 @@
 #!/usr/bin/env perl
-# match00.pl -- Solve matching problem for Sortable.
+# match.pl -- Solve matching problem for Sortable.
 
 use Modern::Perl 2013;
 use autodie;
 use JSON::PP;
 use Data::Dumper;
+use Getopt::Std;
 
 use constant PROD_TYPE_CAMERA => 0;
 use constant PROD_TYPE_ACCESSORY => 1;
 use constant PROD_TYPE_UNKNOWN => 2;
 use constant TEST_MODE => 0;
 
+use constant DATA_PRODUCTS_TEXT => "data/products.txt";
+use constant DATA_LISTINGS_TEXT => "data/listings.txt";
+use constant RESULTS_TEXT => "results.txt";
+
 MAIN:
 {
-    if ( TEST_MODE )
+    my %options = ( p => DATA_PRODUCTS_TEXT, l => DATA_LISTINGS_TEXT,
+                    r => RESULTS_TEXT );
+    getopt("tp:l:r:h", \%options);
+    if ( exists($options{h}) )
+    {
+        help();
+    }
+    elsif ( exists($options{t}) )
     {
         testMatching();
     }
     else
     {
-        my $products = getJsonText("data/products.txt");
-        my $listings = getJsonText("data/listings.txt");
-        matchProductsListings($products, $listings);
+        my $products = getJsonText($options{p});
+        my $listings = getJsonText($options{l});
+        my $results = {};
+        matchProductsListings($products, $listings, $results);
+        outputJsonResults($results, $options{r});
     }
 }
 
 
+# help() -- Print command-line help.
+sub help
+{
+    say STDERR "usage:  match.pl [-t] [-p <prod>] [-l <listing>] [-r <results>] [-h]";
+    say STDERR "  where -t indicates to run test";
+    say STDERR "        <prod> is alternative product file, default ", DATA_PRODUCTS_TEXT;
+    say STDERR "        <listing> is alternative product file, default ", DATA_LISTINGS_TEXT;
+    say STDERR "        <results> is alternative results file, default ", RESULTS_TEXT;
+    say STDERR "        -h gives this help message";
+}
+
+
+# getJsonText() -- Load Json text file into data structure and return the structure.
+# Structure returned is reference to list of records.  Content of that structure
+# is determined by input file.
 sub getJsonText
 {
     my ( $fname ) = @_;
@@ -41,9 +70,39 @@ sub getJsonText
 }
 
 
+# outputJsonResults() -- Dump results structure in json format to given file
+# name.
+# Parameters:
+#   $results:  results structure
+#   $fname:  file name for json output
+sub outputJsonResults
+{
+    my ($results, $fname) = @_;
+    open(my $fh, ">", $fname) or die "Couldn't open file: $!";
+    my $json = JSON::PP->new;
+    $json->pretty;
+    $json->canonical;
+    while ( my ( $product_name, $listing_array ) = each(%$results) )
+    {
+        my $json_record = { product_name => $product_name,
+                            listings => $listing_array };
+        print $fh $json->encode($json_record), "\n";
+    }
+    close $fh;
+}
+
+
+# matchProductsListings() -- Load json structures and perform matching
+# heuristics between listings and products.  Log results where appropriate,
+# and build results structure.
+# This is the top level function.
+# Parameters:
+#   $products:  products as loaded from json file
+#   $listings:  listings as loaded from json file
+#   $results:  results structure (built here), ready for output to json file
 sub matchProductsListings
 {
-    my ( $products, $listings ) = @_;
+    my ( $products, $listings, $results ) = @_;
     my %manuf_prod_list;
     my %prod_mfg_list;
     my %lstg_mfg_list;
@@ -69,9 +128,11 @@ sub matchProductsListings
         $manuf_prod_list{$manuf} = []  unless exists( $manuf_prod_list{$manuf} );
         my $mpl = $manuf_prod_list{$manuf};
         push(@$mpl, $i);
+        $results->{$prod->{product_name}} = [];
         #print Dumper($prod);
     }
-    my $report_stats = { n_none => 0, n_cam_1 => 0, n_cam_n => 0, n_acc_1 => 0, n_acc_n => 0, n_both => 0, n_reason_no_manuf => 0 };
+    my $report_stats = { n_none => 0, n_cam_1 => 0, n_cam_n => 0,
+                         n_reason_no_manuf => 0 };
     #foreach my $lstg ( @$listings )
     for ( my $i = 0;  $i<@$listings;  ++$i )
     {
@@ -81,10 +142,10 @@ sub matchProductsListings
         my $manuf = $mfg_mapping->{$lstg->{manufacturer}};
         #say "manuf==$manuf";
         #print Dumper(\%manuf_prod_list);
-        my ( $n_cam, $n_acc ) = ( 0, 0 );
         if ( exists($manuf_prod_list{$manuf}) )
         {
-            processProdCandidate($products, $manuf_prod_list{$manuf}, $i, $lstg, $report_stats);
+            processProdCandidate($products, $manuf_prod_list{$manuf}, $i,
+                                 $lstg, $report_stats, $results);
         }
         else
         {
@@ -98,61 +159,62 @@ sub matchProductsListings
     say "    Number because of no manufacturer:  $report_stats->{n_reason_no_manuf}";
     say "  Number of listings matching 1 camera only:  $report_stats->{n_cam_1}";
     say "  Number of listings matching >1 camera only:  $report_stats->{n_cam_n}";
-    say "  Number of listings matching 1 accessory only:  $report_stats->{n_acc_1}";
-    say "  Number of listings matching >1 accessory only:  $report_stats->{n_acc_n}";
-    say "  Number of listings matching both camera(s) and accessory(s):  $report_stats->{n_both}";
 }
 
 
+# processProdCandidate() -- Take list of products, listing item and other
+# values, and try matching listing item against product(s).
+# Report results.
+# Precondition:  manufacturer field for listing item has already been checked
+# for existence.
+# Parameters:
+#   $products:  list reference containing product records loaded from json
+#   $manuf_prod_list:  List of indices in $products corresponding to listing
+#           item's manufacturer.
+#   $lstg_index:  index in listing list
+#   $lstg:  record contents from listing list
+#   $report_stats:  statistics reporting structure, to be passed through to
+#           appropriate routines for accumulating statistics on match results
+#   $results:  results structure to be made ready for final json output;
+#           this is a hash indexed by product name and containing for each
+#           a list of listing objects that match
 sub processProdCandidate
 {
-    my ( $products, $manuf_prod_list, $lstg_index, $lstg, $report_stats ) = @_;
+    my ( $products, $manuf_prod_list, $lstg_index, $lstg, $report_stats, $results ) = @_;
     #print Dumper($manuf_prod_list, $lstg);
     my $lstg_title = $lstg->{title};
     #say "lstg_title==$lstg_title";
     my @camera_prod_ind_list;
-    my @accessory_prod_ind_list;
     foreach my $prod_ind ( @$manuf_prod_list )
     {
         my $prod = $products->[$prod_ind];
         my $prod_model = $prod->{model};
         my $prod_family = $prod->{family};
-        #say "  prod_ind==$prod_ind, prod_model==($prod_model)";
-        #if ( index($lstg_title, $prod_model)>=0 or
-        #     index($lstg_title, $prod_family)>=0 )
-        my $orig_match = origMatchCheck($prod, $lstg);
         my $does_match = doesMatch($prod, $lstg);
-        #if ( doesMatch($prod, $lstg) )
-        my $same_match = $orig_match==$does_match ? 1 : 0;
-        #say "Match results:  same_match==$same_match, prod_ind==$prod_ind, lstg_index==$lstg_index, orig_match==$orig_match, does_match==$does_match";
+        #say "Match results:  prod_ind==$prod_ind, lstg_index==$lstg_index, does_match==$does_match";
         if ( $does_match )
         {
             #say "  match:  prod_model==($prod_model)";
             #say "  lstg_title==($lstg_title)";
-            my $class = classifyListing($lstg);
-            #say "class==$class";
-            if ( $class==PROD_TYPE_CAMERA )
-            {
-                #say "  match (camera):";
-                #say "    prod_model==($prod_model)";
-                #say "    lstg_title==($lstg_title)";
-                push(@camera_prod_ind_list, $prod_ind);
-            }
-            elsif ( $class==PROD_TYPE_ACCESSORY )
-            {
-                push(@accessory_prod_ind_list, $prod_ind);
-            }
+            push(@camera_prod_ind_list, $prod_ind);
+            my $res_item = $results->{$prod->{product_name}};
+            push(@$res_item, $lstg);
         }
     }
     #say "  camera_prod_ind_list==(@camera_prod_ind_list)";
-    #say "  accessory_prod_ind_list==(@accessory_prod_ind_list)";
-    reportListing($products, $lstg, \@camera_prod_ind_list, \@accessory_prod_ind_list, $report_stats);
+    reportListing($products, $lstg, \@camera_prod_ind_list, $report_stats);
 }
 
 
+# reportListing() -- Report on listing and anything found for it.  Accumulate statistics.
+# Parameters:
+#   $products:  list of all product structures as loaded from json
+#   $lstg:  listing entry
+#   $camera_prod_ind_list:  list of product indices for cameras
+#   $reportListing:  statistics accumulation structure
 sub reportListing
 {
-    my ($products, $lstg, $camera_prod_ind_list, $accessory_prod_ind_list, $report_stats) = @_;
+    my ($products, $lstg, $camera_prod_ind_list, $report_stats) = @_;
     say "Listing  $lstg->{title}";
     say "  Currency $lstg->{currency}, Price $lstg->{price}";
     say "  Matching cameras:";
@@ -163,41 +225,29 @@ sub reportListing
         say "    model $prod->{model}, name $prod->{product_name}";
         ++$n_cam;
     }
-    say "  Matching accessories:";
-    my $n_acc = 0;
-    foreach my $acc ( @$accessory_prod_ind_list )
-    {
-        my $prod = $products->[$acc];
-        say "    model $prod->{model}, name $prod->{product_name}";
-        ++$n_acc;
-    }
-    say "  Zero match"  if ( $n_cam+$n_acc==0 );
-    say "  Dual match"  if ( $n_cam+$n_acc>1 );
-    ++$report_stats->{n_none}  if ( $n_cam==0 && $n_acc==0 );
-    ++$report_stats->{n_cam_1}  if ( $n_cam==1 && $n_acc==0 );
-    ++$report_stats->{n_cam_n}  if ( $n_cam>1 && $n_acc==0 );
-    ++$report_stats->{n_acc_1}  if ( $n_acc==1 && $n_cam==0 );
-    ++$report_stats->{n_acc_n}  if ( $n_acc>1 && $n_cam==0 );
-    ++$report_stats->{n_both}  if ( $n_cam>0 && $n_acc>0 );
+    say "  Zero match"  if ( $n_cam==0 );
+    say "  Dual match"  if ( $n_cam>1 );
+    ++$report_stats->{n_none}  if ( $n_cam==0 );
+    ++$report_stats->{n_cam_1}  if ( $n_cam==1 );
+    ++$report_stats->{n_cam_n}  if ( $n_cam>1 );
 }
 
 
-sub classifyProduct
-{
-    my ( $prod ) = @_;
-    return PROD_TYPE_CAMERA;
-}
-
-
-sub classifyListing
-{
-    my ( $lstg ) = @_;
-    my $type = PROD_TYPE_CAMERA;
-    $type = PROD_TYPE_ACCESSORY  if ( exists($lstg->{price}) and $lstg->{price}<=75.0 );
-    return $type;
-}
-
-
+# createManufacturerMapping() -- Take list of product manufacturer values, and
+# list of listing manufacturer values.  Figure out which manufacturer names in
+# the listings correspond to which manufacturer names in the products.  Return
+# hash reference giving this mapping:  input listing manufacturer name, get
+# back product manufacturer name.
+# Parameters:
+#   prod_mfg_keys:  list reference of product manufacturer fields
+#   lstg_mfg_keys:  list reference of listing manufacturer fields
+#
+# Explanation:
+# Product records will have manufacturer names like "Canon" or "Nikon".
+# Listing records will have manufacturer names like "Canon Canada" or
+# "Nikon PLC".
+# This function applies simple string indexing rule to match them, and keeps
+# results in map.
 sub createManufacturerMapping
 {
     my ($prod_mfg_keys, $lstg_mfg_keys) = @_;
@@ -215,6 +265,8 @@ sub createManufacturerMapping
 }
 
 
+# testMatching()
+# Apply various test cases to determine how well doesMatch() performs.
 sub testMatching
 {
     if ( 0 )
@@ -293,30 +345,20 @@ sub testMatching
 }
 
 
-sub origMatchCheck
-{
-    my ( $prod, $lstg ) = @_;
-    my $ok = 0;
-    #$ok = 1  if ( (exists($prod->{model}) and index($lstg->{title}, $prod->{model})>=0) or
-    #              index($lstg->{title}, $prod->{family})>=0 );
-    $ok = 1  if ( (exists($prod->{model}) and index($lstg->{title}, $prod->{model})>=0) );
-    $ok = 1 if ( (not $ok) and (exists($prod->{family}) and index($lstg->{title}, $prod->{family})>=0) );
-    return $ok;
-}
-
-
-# Assume that match on manufacturer has already been confirmed.
+# doesMatch() -- Apply heuristics to determine whether given listing entry
+# matches given product entry.
+# Precondition:  match on manufacturer has already been confirmed.
 sub doesMatch
 {
     my ( $prod, $lstg ) = @_;
     my $ok = 1;
-    my $family_ok = 1; # Whether family is ignored in description is kind of fuzzy.
-    if ( exists($prod->{family}) )
-    {
-        my $pat = "$prod->{family}";
-        $family_ok = 0  unless ( $lstg->{title} =~ m/\b$pat\b/ );
-        #say "pat 0 ($pat), family_ok==$family_ok";
-    }
+    #my $family_ok = 1; # Whether family is ignored in description is kind of fuzzy.
+    #if ( exists($prod->{family}) )
+    #{
+    #    my $pat = "$prod->{family}";
+    #    $family_ok = 0  unless ( $lstg->{title} =~ m/\b$pat\b/ );
+    #    #say "pat 0 ($pat), family_ok==$family_ok";
+    #}
     if ( exists($prod->{model}) )
     {
         my $pat = "$prod->{model}";
@@ -363,6 +405,8 @@ sub doesMatch
 }
 
 
+# extendedModelPattern() -- Utility to expand camera model string to one that
+# matches a suffix context.
 sub extendedModelPattern
 {
     my ( $pat ) = @_;
@@ -374,6 +418,9 @@ sub extendedModelPattern
 }
 
 
+# allowSuffix():  Determine answer to following question for given
+# manufacturer.
+# Question:
 # Should suffix be allowed as same model?
 # For some manufacturers, the answer is yes.  For example, the Sony DSC-T99/B
 # or DSC-T99B appears to be just a later minor edition of the T99, whereas
